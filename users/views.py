7b2278bad627cd061e64e4dadacaf4aca28ae29d
PandaSuzuki <renatosuzuki03@gmail.com>
2022-11-03 09:46:40 -0300

    feat: added register, login, get all and settings

--- users/views.py
+++ users/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate
+from rest_framework.views import Request, Response, status, APIView
+from rest_framework import generics
+from rest_framework.authtoken.models import Token
+from .serializers import UserSerializer, LoginSerializer
+from .models import User
 
-# Create your views here.
+
+class UserView(generics.ListCreateAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+
+class LoginView(APIView):
+    def post(self, request: Request) -> Response:
+        serializer = LoginSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        user = authenticate(**serializer.validated_data)
+
+        if user:
+            token, _ = Token.objects.get_or_create(user=user)
+
+            return Response({"token": token.key}, status.HTTP_200_OK)
+
+        return Response({"detail": "wrong username or password"}, status.HTTP_400_BAD_REQUEST)
