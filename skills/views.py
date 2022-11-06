b358583755c0872073fb78b191711eafa0c688ce
Bruno Gomes <gomesbruno030@outlook.com>
2022-11-06 03:13:28 -0300

    feat test skill view

--- skills/views.py
+++ skills/views.py
@@ -2,18 +2,30 @@ from django.shortcuts import render
 from rest_framework.generics import ListAPIView
 from rest_framework import generics
 
-# from rest_framework.permissions import IsAuthenticated
-# from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from .permissions import So_adm
+from rest_framework.authentication import TokenAuthentication
 
 from .models import Skill
 from .serializers import Skill_Serializer
 
-class SkillsViews(generics.ListCreateAPIView):
+class SkillsGetViews(generics.ListCreateAPIView):
+    queryset = Skill.objects.all()
+    serializer_class = Skill_Serializer
+
+class SkillsCreateViews(generics.ListCreateAPIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [So_adm]
     queryset = Skill.objects.all()
     serializer_class = Skill_Serializer
 
 class SkillsViewsbyId(generics.RetrieveUpdateDestroyAPIView):
-    # authentication_classes = [TokenAuthentication]
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [So_adm]
+    queryset = Skill.objects.all()
+    serializer_class = Skill_Serializer
+
+class SkillsGetViewsbyId(generics.RetrieveUpdateDestroyAPIView):
     queryset = Skill.objects.all()
     serializer_class = Skill_Serializer
 
