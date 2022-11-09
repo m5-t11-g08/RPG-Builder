52d51c5747e01cf510c3c34df63a08dbbd8b0f88
PandaSuzuki <renatosuzuki03@gmail.com>
2022-11-09 18:17:28 -0300

    feat: update attributes

--- characters/serializers.py
+++ characters/serializers.py
@@ -5,12 +5,26 @@ from classes.models import Class
 from equipments.serializers import EquipmentSerializer
 from skills.serializers import Skill_Serializer
 from .errors import Class404
+from attributes.models import Attribute
+
+class AttributeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Attribute
+        fields = [
+            "id",
+            "life",
+            "attack",
+            "defense",
+            "mana"
+        ]
+
 
 class CharacterSerializer(serializers.ModelSerializer):
     char_class_id = serializers.CharField(write_only=True)
     char_class = ClassSerializer(read_only=True)
     equipments = EquipmentSerializer(many=True, read_only=True)
     skills = Skill_Serializer(many=True, read_only=True)
+    char_attribute = AttributeSerializer(read_only=True)
 
     class Meta:
         model = Character
@@ -25,11 +39,12 @@ class CharacterSerializer(serializers.ModelSerializer):
             "user_id",
             "equipments",
             "skills",
+            "char_attribute"
         ]
 
         read_only_fields = [
             "equipments",
-            "skills",
+            "skills"
         ]
     # char_class = CharClassSerializer
     # equipments = EquipmentsSerializer
